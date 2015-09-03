8b7a74228bdda67f61eadf2d853c3dcda8904b15
Karsten Kamm <karsten.kamm@phimobile.com>
2015-09-03 17:52:52 +0200

    collision detection, ball deflection

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ Layer *ball_layer;
 
 AppTimer *timer;
 
-const int delta = 40;
+const int delta = 20;
 int dx = 1;
 
 int timercallback_max_duration = 0;
@@ -25,14 +25,19 @@ const int ball_initial_y = 84;
 const int paddle_initial_y =84;
 const int paddle_step = 15;
 
+const int speed_increment = 1.03;
+
 //game state
 int millis_current = 0;
+int millis_between_updates = 0;
 float speed;
 float ball_x;
 float ball_y;
 float ball_velocity_x;
 float ball_velocity_y;
 float paddle_y;
+
+int game_number_of_hits ;
 
 
 bool game_has_started = false;
@@ -41,11 +46,55 @@ bool game_has_started = false;
 void init_game_state() {
   ball_x = ball_initial_x;
   ball_y = ball_initial_y;
-  ball_velocity_x = 1.0/40;
+  ball_velocity_x = 2.0/40;
   ball_velocity_y = 0.3/40;
   speed = 1.0;
+  game_number_of_hits = 0;
   
   paddle_y = paddle_initial_y;
+}
+
+void game_reset() {
+  game_has_started = false;
+  init_game_state();
+  layer_set_frame(text_layer_get_layer(paddle_layer), GRect(0,paddle_y,paddle_width,paddle_height));
+  text_layer_set_text(text_layer, "");
+  text_layer_set_text(debug_text_layer,"");
+}
+
+
+float my_sqrt( float num ) {
+  float a;
+  float p;
+  float e = 0.01;
+  float b;
+
+  a = num;
+  p = a * a;
+  while( p - num >= e ) {
+    b = ( a + ( num / a ) ) / 2;
+    a = b;
+    p = a * a;
+  }
+  return a;
+}
+
+void ball_deflect(int over) {
+  //float fraction = ((float)over) / ball_radius;
+  float current_ball_speed = my_sqrt(ball_velocity_x*ball_velocity_x + ball_velocity_y*ball_velocity_y);
+  float current_velocity_y = ball_velocity_y;
+  
+  //TODO this is not really correct
+  ball_velocity_y = ball_velocity_y + 50*current_ball_speed*over/ball_radius;
+  
+  float new_ball_speed = my_sqrt(ball_velocity_x*ball_velocity_x + ball_velocity_y*ball_velocity_y);
+  float corrective_factor = current_ball_speed / new_ball_speed;
+  
+  ball_velocity_x = ball_velocity_x * corrective_factor;
+  ball_velocity_y = ball_velocity_y * corrective_factor;
+  
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "hit: %d , over %d from %d to %d",(int)(current_ball_speed*100000), over,
+          (int)(10000*current_velocity_y), (int)(10000*ball_velocity_y));
 }
 
 void ball_update_proc (Layer *layer, GContext *ctx) {
@@ -69,10 +118,11 @@ void timer_callback(void *data) {
     game_has_started = true;
   } else {
     
-    elapsed = time1_millis - millis_current;
+    elapsed = time1_millis - millis_between_updates;
     if (elapsed < 0) {
       elapsed = 1000 + elapsed;
     }
+        
     static char timestamp_buffer[64];
     snprintf(timestamp_buffer, 64, "elapsed: %d\n", elapsed);
     text_layer_set_text(text_layer, timestamp_buffer);
@@ -87,7 +137,7 @@ void timer_callback(void *data) {
       if (ball_velocity_x > 0) {
         ball_velocity_x = -ball_velocity_x;    //Reverse
       }
-      speed = speed * 1.05;
+      speed = speed * speed_increment;
     }
     else if(ball_x < 0)
     {
@@ -95,7 +145,7 @@ void timer_callback(void *data) {
       if (ball_velocity_x<0) {
         ball_velocity_x = -ball_velocity_x ; //Forwards
       }
-      speed = speed * 1.05;
+      speed = speed * speed_increment;
     }
     
     //Check to see if we have hit the bottom/top edge
@@ -105,7 +155,7 @@ void timer_callback(void *data) {
       if (ball_velocity_y > 0) {
         ball_velocity_y = -ball_velocity_y;    //Reverse
       }
-      speed = speed * 1.05;
+      speed = speed * speed_increment;
     }
     else if(ball_y < 0)
     {
@@ -113,12 +163,39 @@ void timer_callback(void *data) {
       if (ball_velocity_y < 0) {
         ball_velocity_y = -ball_velocity_y; //Forwards
       }
-      speed = speed * 1.05;
+      speed = speed * speed_increment;
     }
     
-    //TODO check if we hit paddle
+    //check if we hit paddle
+    if(ball_x == 0) {
+//      APP_LOG(APP_LOG_LEVEL_DEBUG, "edge reached. ball at %d, paddle from %d to %d",
+//              (int)ball_y, (int)paddle_y, (int)(paddle_y+paddle_height));
+
+      if ((ball_y+ball_radius > paddle_y - ball_radius) && (ball_y+ball_radius < paddle_y + paddle_height + ball_radius)) {
+        // ball has hit paddle
+        
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "ball has hit paddle");        
+        
+        game_number_of_hits++;
+
+        int distance_top = ball_y+ball_radius - paddle_y;
+        int distance_bottom = paddle_y+paddle_height - (ball_y+ball_radius);
+        if (distance_top < 0) {
+          ball_deflect (distance_top);
+        }
+        if (distance_bottom < 0) {
+          ball_deflect(-distance_bottom);
+        }
+        static char string_buffer[64];                
+        snprintf(string_buffer, 64, "n: %d, x: %d y:%d", game_number_of_hits, (int)(ball_velocity_x*10000), (int)(10000*ball_velocity_y));
+        text_layer_set_text(debug_text_layer, string_buffer);
+      } else {
+        game_reset();
+      }
+    } 
+      
     
-    //APP_LOG(APP_LOG_LEVEL_DEBUG, "new velocity: %d ",(int)ball_velocity_x*1000);
+    
     
     ball_x = ball_x + ball_velocity_x * elapsed * speed;
     ball_y = ball_y + ball_velocity_y * elapsed * speed;
@@ -127,7 +204,11 @@ void timer_callback(void *data) {
     GRect ball_current_frame = layer_get_frame(ball_layer);
     GRect ball_next_frame = GRect(ball_x, ball_y, ball_current_frame.size.w, ball_current_frame.size.w);
     layer_set_frame(ball_layer, ball_next_frame);
+    
+    //debug / FIXME
+    //psleep(rand()%550);
   }
+  millis_between_updates = (int) time1_millis;
   
   //Get time at end of app loop callback
   time_t time_end_seconds;
@@ -142,7 +223,7 @@ void timer_callback(void *data) {
     diff = 1000 + diff;
   }
     
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "game loop took %ld", diff); 
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "game loop took %ld", diff); 
   int time_until_next_frame = 0;
   if (delta-diff > 0) {
     time_until_next_frame = delta - diff;
@@ -156,17 +237,14 @@ void timer_callback(void *data) {
 
 // Buttons
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(debug_text_layer, "Up pressed!");
   paddle_y = paddle_y - paddle_step;
   layer_set_frame(text_layer_get_layer(paddle_layer), GRect(0,paddle_y,paddle_width,paddle_height));
 }
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(debug_text_layer, "Select pressed!");
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(debug_text_layer, "Down pressed!");
   paddle_y = paddle_y + paddle_step;
   layer_set_frame(text_layer_get_layer(paddle_layer), GRect(0,paddle_y,paddle_width,paddle_height));
 }
@@ -204,6 +282,8 @@ void window_load(Window *window)
   paddle_layer = text_layer_create(GRect(0, 50, paddle_width, paddle_height));
   text_layer_set_background_color(paddle_layer, GColorBlack);
   text_layer_set_text_color(paddle_layer, GColorBlack);
+  layer_set_frame(text_layer_get_layer(paddle_layer), GRect(0,paddle_y,paddle_width,paddle_height));
+  
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(paddle_layer));
   
  
@@ -228,6 +308,7 @@ void window_unload(Window *window)
 
 void init()
 {
+  
 //Initialize the app elements here!
   window = window_create();
   window_set_fullscreen(window, true);
